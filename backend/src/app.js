296a478d60a2b27f673aca80707917dcacca4f11
joaogabrielvalenca <joaogabrielvalenca@gmail.com>
2023-07-04 11:15:18 -0300

    iniciando projeto store manager

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,7 +4,7 @@ const app = express();
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
-  response.json({ status: 'Store Manager UP!' });
+  response.json({ status: 'Store Manager UP!!' });
 });
 
 module.exports = app;
